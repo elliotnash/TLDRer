8010d7afcad51d3cdacee1a50a7f12c48b5910de
Elliot Nash <elliotnash@gmail.com>
2023-08-06 19:43:06 -0700

    Add rpc queue

--- src/main/kotlin/services/signal/SignalService.kt
+++ src/main/kotlin/services/signal/SignalService.kt
@@ -1,7 +1,7 @@
 package services.signal
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
+// import kotlinx.coroutines.future.
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonArray
@@ -12,6 +12,10 @@ import services.ChatService
 import services.signal.jsonrpc.RpcCall
 import services.signal.jsonrpc.RpcMessage
 import services.signal.jsonrpc.RpcMessageSerializer
+import services.signal.jsonrpc.RpcResult
+import java.io.BufferedReader
+import java.util.Scanner
+import java.util.UUID
 
 class SignalService : ChatService {
     private val json = Json {
@@ -23,20 +27,41 @@ class SignalService : ChatService {
     private val process = ProcessBuilder("signal-cli", "jsonRpc").start()
     private val reader = process.inputStream.reader()
     private val writer = process.outputStream.writer()
-    private fun sendRpcMessage(message: RpcMessage) {
-        writer.write(json.encodeToString(message)+"\n")
-        writer.flush()
+
+    private val rpcQueue = mutableMapOf<String, CompletableDeferred<RpcResult>>()
+    private suspend fun sendRpcMessage(message: RpcCall): RpcResult {
+        // get request ID
+        val id = message.id ?: UUID.randomUUID().toString()
+        // Add completable to queue
+        rpcQueue[id] = CompletableDeferred()
+        // Send request
+        withContext(Dispatchers.IO) {
+            writer.write(json.encodeToString(message)+"\n")
+            writer.flush()
+        }
+        return rpcQueue[id]!!.await()
     }
     override fun start(scope: CoroutineScope) {
         // Reader coroutine
         scope.launch {
-            reader.forEachLine {
-                val message = json.decodeFromString<RpcMessage>(it)
-                println(message)
+            withContext(Dispatchers.IO) {
+                reader.forEachLine {
+                    val message = json.decodeFromString<RpcMessage>(it)
+                    if (message is RpcResult) {
+                        // If it's a result, we need to complete the rpcQueue
+                        rpcQueue.remove(message.id)?.complete(message)
+                    } else if (message is RpcCall) {
+                        // Else this is a message received
+                        println(message)
+                    }
+                }
             }
         }
+        scope.launch {
+             sendMessage(listOf("+12508807560"), "Testing sendMessage function 3")
+        }
     }
-    fun sendMessage(recipients: List<String>, message: String) {
+    suspend fun sendMessage(recipients: List<String>, message: String) {
         val call = RpcCall(
             "send",
             JsonObject(
